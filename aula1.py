4e6ca8ac30ddd96fd3566454ef86760589dccb7d
rosa7788 <rosennigt7@gmail.com>
2025-09-05 22:18:18 -0300

    jfjhfkjhsdfjkd

---
+++ aula1.py
@@ -0,0 +1,109 @@
+    
+    
+"""
+x = 10
+
+print("x vale:", x)
+
+print(type(x))
+
+y = 33.3
+print(type(y))
+
+a = " facef"
+print(type(a))
+#
+idade = 23
+altura = 1.76
+
+
+print('A altura é:', altura, 'A idade é:', idade)
+print(f'A idade {idade} e a altura {altura}')
+
+#
+nome = input ("digite seu nome:")
+print("Seu nome é:", nome)
+
+numero = input('Digite numero:')
+print("Numero é:", numero)
+print(type(numero))
+
+#
+a = 2.33
+#conversao de numero para string
+print(str(a))
+
+b = '333'
+print(b)
+#conversao de string para inteiro
+c = int(b) + 2
+print (c)
+#
+
+n = 7 / 3
+print ("n", round(n))
+print ("sem arreddondar", n)
+
+import math
+print(math.e) 
+print(math.pi) #valor pi
+print(math.sqrt(2)) #raiz quadrada
+print(dir(math))
+print(dir())
+
+if x > 0:
+    print("Positivo")
+print("Fim do programa")
+
+
+x = int(input("Digite um número:"))
+if x % 2 == 0:
+    print("O valor", x, "É par")
+print("Fim do programa")
+"""
+"""
+nota = int(input("Digite a nota do aluno:"))
+if nota >= 30 and nota < 60:
+    print ("Precisa fazer sub por sua nota ser:", nota)
+else:
+    print("Não precisa de sub")
+"""
+"""
+x = int(input("Digite um valor inteiro:"))
+if x % 2 == 0:
+    print("O valor", x, "é par")
+else:
+    print ("O valor", x, "é ímpar")
+
+print("Fim do programa")
+"""
+"""
+x = int(input("Digite um valor inteiro:"))
+if x == 0:
+    print("O valor", x, "é igual a zero")
+else: 
+    if x > 0:
+        print("O valor", x, "é positivo")
+    else:
+        print("O valor", x, "é negativo")
+
+print("Fim do programa")
+"""
+
+x = int(input("Digite um valor inteiro:"))
+if x == 0:
+    print("O valor", x, "é igual a zero")
+
+elif x > 0:
+        print("O valor", x, "é positivo")
+else:
+        print("O valor", x, "é negativo")
+
+print("Fim do programa")
+
+
+
+
+
+
+
