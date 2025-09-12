b6d422a36026392547ec5504c114df775190a506
rosa7788 <rosennigt7@gmail.com>
2025-09-12 20:36:31 -0300

    exe

---
+++ enquanto.py
@@ -0,0 +1,25 @@
+"""
+a = int(input("Digite o valor de a:"))
+b = int(input("Digite o valor de b:"))
+while a <= b:
+    print(a)
+    a = a + 1
+
+print("Fim do programa")
+"""
+"""
+n = int(input("Digite o numero:"))
+media = 0
+total = 0
+
+while total < n:
+    x = int(input("Digite o valor x:"))
+    media = media + x
+    total = total + 1
+media = media/n
+
+print("Media", media)
+ """
+
+
+
---
+++ para.py
@@ -0,0 +1,21 @@
+"""
+for n in [10,30,55,-1,15]:
+    print(n)
+"""
+"""
+lista = [10,30,55,11,-1,15]
+
+for n in lista:
+    print(n)
+    soma += n
+
+print("soma da lista:", soma)
+
+compras = ["Miojo", "Ovo", "Leite", "Pão"]
+print("Lista de produtos")
+for item in compras:
+    print ("Produto:", item)
+"""
+for n in range(4,10):
+    print(n)
+
