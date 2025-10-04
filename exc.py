d37500d9cd79c743f2e3d1b990a951b7de217715
rosa7788 <rosennigt7@gmail.com>
2025-10-03 22:15:05 -0300

    exe

--- exc.py
+++ exc.py
@@ -28,6 +28,8 @@ else:
     print("Parcela:", parcela)
     print("Desconto:", desconto)
 
+    
+
 
 
 
--- para.py
+++ para.py
@@ -16,8 +16,28 @@ print("Lista de produtos")
 for item in compras:
     print ("Produto:", item)
 """
+"""
 for n in range(4,10):
     print(n)
+"""
+"""
+valores = [10, 20, 30]
+for x in valores:
+    print(x)
+"""
+"""
+nomes = [Rosa, Maria, Vitoria]
+for nome in nomes:
+    print(nome)
+"""
+n = int(input("Informe numero para a tabuada:"))
+
+for x in range (1,11):
+    total = (x * n)
+    
+    print ("A tabuada é:", n, "X", x, "=", total)
+
+
 
 
 
